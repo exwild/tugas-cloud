6a40caedbcbbaaa1ea7aca0f73a8746d7eb7fb80
exwild <62737756+exwild@users.noreply.github.com>
2020-03-28 13:18:17 +0700

    Update catch.php

--- catch.php
+++ catch.php
@@ -17,7 +17,7 @@ if(isset($_POST)){
 	$password = 'ubaya';
 	$dbname = 'tugasisadb';
 
-	$conn = new mysqli_connect($servername, $username, $password, $dbname);
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
